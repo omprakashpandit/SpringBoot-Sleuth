f1c0c0b7d6bb91936a1216b807f3bc1fcfab7444
omprakashpandit <32616762+omprakashpandit@users.noreply.github.com>
2022-10-17 10:33:39 +0530

    Add files via upload

---
+++ SpringBoot-Sleuth-Server/src/main/java/com/ram/Application.java
@@ -0,0 +1,17 @@
+package com.ram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+//SpringBootDemo,224b329f6ddb8c10,58b3d3b63faf4af2,false
+//SpringBootRestTemplateDemo,224b329f6ddb8c10,224b329f6ddb8c10,false
